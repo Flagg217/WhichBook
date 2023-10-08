9597bfddd2f23b958e03304e9e74cac18d3df5af
Flagg217 <flagg217@gmail.com>
2023-10-08 16:33:03 -0700

    added more mutations to resolvers

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -36,7 +36,32 @@ const resolvers = {
         const token = signToken(user);
         return { token, user };
     },
+    saveBook: async (parent, args) => {
+        console.log(user);
+    try {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $addToSet: { savedBooks: args } },
+        { new: true, runValidators: true }
+      );
+      return res.json(updatedUser);
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json(err);
+    }
   },
+  deleteBook: async (parent, { bookId }) => {
+    const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: bookId } } },
+        { new: true }
+      );
+      if (!updatedUser) {
+        return res.status(404).json({ message: "Couldn't find user with this id!" });
+      }
+      return res.json(updatedUser);
+    },
+},
 };
 
 module.exports = resolvers;
